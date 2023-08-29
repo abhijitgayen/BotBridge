fbd06a57d46773fe862cffcdb54e7f299d62e84f
abhijitCogoport <abhijit.gayen@cogoport.com>
2023-08-29 23:57:28 +0530

    cors error fixes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+var cors = require('cors')
 const app = express();
+app.use(cors())
 app.use(express.json())
 const {getBotResponse} = require('./botResponse');
 
